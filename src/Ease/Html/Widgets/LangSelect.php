363c98d5391a9152d25dc658756e110372121463
CyberVitexus <info@vitexsoftware.cz>
2024-10-12 22:23:36 +0200

    Modernization added configurable lang select name

--- src/Ease/Html/Widgets/LangSelect.php
+++ src/Ease/Html/Widgets/LangSelect.php
@@ -15,9 +15,9 @@ namespace Ease\Html\Widgets;
  */
 class LangSelect extends \Ease\Html\SelectTag
 {
-    public function __construct($properties = [])
+    public function __construct($name = 'lang', $properties = [])
     {
-        parent::__construct(null, $properties);
+        parent::__construct($name, $properties);
         $locale = \Ease\Locale::singleton();
         foreach ($locale->availble() as $code => $name) {
             $name = substr($name, 0, strpos($name, ' ('));
