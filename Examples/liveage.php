8b21655c837b0caa3c736506c034680801dd5e22
CyberVitexus <info@vitexsoftware.cz>
2024-11-16 15:16:08 +0100

    v1.1.0 release

---
+++ Examples/liveage.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the EaseHtmlWidgets package
+ *
+ * https://github.com/VitexSoftware/php-vitexsoftware-ease-html-widgets
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once '../vendor/autoload.php';
+
+$oPage = new \Ease\WebPage();
+
+$oPage->addItem(new \Ease\Html\Widgets\Toggle('tog1', 'a'));
+$oPage->addItem(new \Ease\Html\Widgets\ToggleRounded('tog2', 'b'));
+
+echo $oPage;
